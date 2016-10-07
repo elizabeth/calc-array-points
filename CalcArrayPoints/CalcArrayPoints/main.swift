cbc0c5a24e089ba3a1906a21bcdd5115cbffa9df
luludust <elizabethduong96@gmail.com>
2016-10-06 19:07:01 -0700

    Initial code

---
+++ CalcArrayPoints/CalcArrayPoints/main.swift
@@ -0,0 +1,30 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by iGuest on 10/6/16.
+//  Copyright © 2016 Elizabeth. All rights reserved.
+//
+
+import Foundation
+
+// Build a calculator
+func add() {
+    
+}
+
+func subtract() {
+    
+}
+
+func multiply() {
+    
+}
+
+func divide() {
+    
+}
+
+func math(first: Int, second: Int, op: (Int, Int) -> Double) -> Double {
+    return op(first, second)
+}

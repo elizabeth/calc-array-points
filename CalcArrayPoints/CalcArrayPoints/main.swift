15fab166c9ebb81ed9f8306d42e2b932ea0a57fd
luludust <elizabethduong96@gmail.com>
2016-10-07 17:48:48 -0700

    Some code

--- CalcArrayPoints/CalcArrayPoints/main.swift
+++ CalcArrayPoints/CalcArrayPoints/main.swift
@@ -9,22 +9,79 @@
 import Foundation
 
 // Build a calculator
-func add() {
-    
+func add(first : Int, second : Int) -> Double {
+    return Double(first + second)
 }
 
-func subtract() {
-    
+func subtract(first : Int, second : Int) -> Double {
+    return Double(first - second)
 }
 
-func multiply() {
-    
+func multiply(first : Int, second : Int) -> Double {
+    return Double(first * second)
 }
 
-func divide() {
-    
+func divide(first : Int, second : Int) -> Double {
+ 	return Double(first / second)   
 }
 
-func math(first: Int, second: Int, op: (Int, Int) -> Double) -> Double {
+func math(first : Int, second : Int, op : (Int, Int) -> Double) -> Double {
     return op(first, second)
+}
+
+// Array fun
+func addArray(nums : Array<Int>) -> Int {
+	var result : Int = 0
+	for num in nums {
+		result += num
+	}
+
+	return result
+}
+
+func multiplyArray(nums : Array<Int>) -> Int {
+	var result : Int = 0
+	for num in nums {
+		result *= num
+	}
+
+	return result
+}
+
+func count(nums : Array<Int>) -> Double {
+	return nums.count	
+}
+
+func avg(nums : Array<Int>) -> Double {
+	var total : Double = 0
+	for num in nums {
+		total += num
+	}	
+
+	return Double(total/nums.count)
+}
+
+func mathArray(numArray : Array<Int>, op : (Array<Int>) -> Int) -> Double {
+	return op(numArray)
+}
+
+// Points
+func addPoint(first :(Int, Int), second(Int, Int)) -> (Int, Int) {
+	return(first.0 + second.0, first.1 + second.1)
+}
+
+func subtractPoint(first : (Int, Int), second(Int, Int)) -> (Int, Int) {
+	return (first.0 - second.0, first.1 - second.1)	
+}
+
+func addDict(first : Dictionary<Double, Double>, second : Dictionary<Double, Double) -> Dictionary<Double, Double> {
+	return ("x" : first.x + second.x, "y" : first.y + second.y)
+}
+
+func subtractDict(first : Dictionary<Double, Double>, second : Dictionary<Double, Double) -> Dictionary<Double, Double> {
+	return ("x" : first.x - second.x, "y" : first.y - second.y)
+}
+
+func mathDict(first : Dictionary<Double, Double>, second : Dictionary<Double, Double>, op : (Dictionary<Double, Double>) -> Double) -> Dictionary<Double, Double> {
+	return op(first, second)
 }

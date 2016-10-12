f605c2ad8ce6ab8d48a9cd35a13126b9c33576c4
Elizabeth Duong <eduong@uw.edu>
2016-10-11 23:55:04 -0700

    Add optionals for arguments

--- CalcArrayPoints/CalcArrayPoints/main.swift
+++ CalcArrayPoints/CalcArrayPoints/main.swift
@@ -9,79 +9,139 @@
 import Foundation
 
 // Build a calculator
-func add(left : Int, right : Int) -> Double {
-    return Double(left) + Double(right)
-}
-
-func subtract(left : Int, right : Int) -> Double {
-    return Double(left) - Double(right)
-}
-
-func multiply(left : Int, right : Int) -> Double {
-    return Double(left) * Double(right)
-}
-
-func divide(left : Int, right : Int) -> Double {
-    return Double(left) / Double(right)
-}
-
-func mathOperation(left : Int?, right : Int?, operation : (Int, Int) -> Double) -> Double {
+func add(left : Int?, right : Int?) -> Double? {
     guard let l = left else {
         print("Must enter valid integers")
-        return 0.0
+        return nil
+    }    
+    
+    guard let r = right else {
+        print("Must enter valid integers")
+        return nil
+    }    
+    
+    return Double(l) + Double(r)
+}
+
+func subtract(left : Int?, right : Int?) -> Double? {
+    guard let l = left else {
+        print("Must enter valid integers")
+        return nil
+    }    
+    
+    guard let r = right else {
+        print("Must enter valid integers")
+        return nil
+    }    
+    
+    return Double(l) - Double(r)
+}
+
+func multiply(left : Int?, right : Int?) -> Double? {
+    guard let l = left else {
+        print("Must enter valid integers")
+        return nil
+    }    
+    
+    guard let r = right else {
+        print("Must enter valid integers")
+        return nil
+    }
+    
+    return Double(l) * Double(r)
+}
+
+func divide(left : Int?, right : Int?) -> Double? {
+    guard let l = left else {
+        print("Must enter valid integers")
+        return nil
+    }    
+    
+    guard let r = right else {
+        print("Must enter valid integers")
+        return nil
+    } 
+    
+    return Double(l) / Double(r)
+}
+
+func mathOperation(left : Int?, right : Int?, operation : (Int?, Int?) -> Double?) -> Double? {
+    guard let l = left else {
+        print("Must enter valid integers")
+        return nil
     }
     
     guard let r = right else {
         print("Must enter valid integers")
-        return 0.0
+        return nil
     }
     
     return operation(l, r)
 }
 
 // Array fun
-func add(array : Array<Int>) -> Double {
+func add(array : Array<Int>?) -> Double? {
+    guard let numArray = array else {
+        print("Must pass an array")
+        return nil
+    }
+    
     var result : Int = 0
-    for num in array {
+    for num in numArray {
         result += num
     }
 
     return Double(result)
 }
 
-func multiply(array : Array<Int>) -> Double {
+func multiply(array : Array<Int>?) -> Double? {
+    guard let numArray = array else {
+        print("Must pass an array")
+        return nil
+    }
+    
     var result : Int = 1
-    if reduce(array : array, operation : count) == 0 {
+    if numArray.count == 0 {
         result = 0
     }
     
-    for num in array {
+    for num in numArray {
         result *= num
     }
 
     return Double(result)
 }
 
-func average(array : Array<Int>) -> Double {
-    let total = add(array: array)
+func average(array : Array<Int>?) -> Double? {
+    guard let numArray = array else {
+        print("Must pass an array")
+        return nil
+    }
     
-    let totalNum = array.count
+    let total = add(array: numArray)
+    
+    let totalNum = numArray.count
     if totalNum == 0 {
         print("Array had no ints")
-        return 0.0
+        return nil
     }
     
     return total/Double(totalNum)
 }
 
-func count(array : Array<Int>) -> Double {
-    return Double(array.count)
-}
-
-func reduce(array : Array<Int>?, operation : (Array<Int>) -> Double) -> Double {
+func count(array : Array<Int>?) -> Double? {
     guard let numArray = array else {
         print("Must pass an array")
-        return 0.0
+        return nil
+    }
+    
+    return Double(numArray.count)
+}
+
+func reduce(array : Array<Int>?, operation : (Array<Int>?) -> Double?) -> Double? {
+    guard let numArray = array else {
+        print("Must pass an array")
+        return nil
     }
     
     return operation(numArray)
@@ -112,22 +172,22 @@ func subtract(p1 : Dictionary<String, Int>?, p2 : Dictionary<String, Int>?) -> D
 func add(p1 : Dictionary<String, Double>?, p2 : Dictionary<String, Double>?) -> Dictionary<String, Double>? {
     guard p1?["x"] != nil else {
         print("Must pass valid dictionary")
-        return ["x": 0, "y": 0]
+        return nil
     }
     
     guard p1?["y"] != nil else {
         print("Must pass valid dictionary")
-        return ["x": 0, "y": 0]
+        return nil
     }
     
     guard p2?["x"] != nil else {
         print("Must pass valid dictionary")
-        return ["x": 0, "y": 0]
+        return nil
     }
     
     guard p2?["y"] != nil else {
         print("Must pass valid dictionary")
-        return ["x": 0, "y": 0]
+        return nil
     }
     
     return ["x" : p1!["x"]! + p2!["x"]!, "y" : p1!["y"]! + p2!["y"]!]
@@ -136,22 +196,22 @@ func add(p1 : Dictionary<String, Double>?, p2 : Dictionary<String, Double>?) -> 
 func subtract(p1 : Dictionary<String, Double>?, p2 : Dictionary<String, Double>?) -> Dictionary<String, Double>? {
     guard p1?["x"] != nil else {
         print("Must pass valid dictionary")
-        return ["x": 0, "y": 0]
+        return nil
     }
     
     guard p1?["y"] != nil else {
         print("Must pass valid dictionary")
-        return ["x": 0, "y": 0]
+        return nil
     }
     
     guard p2?["x"] != nil else {
         print("Must pass valid dictionary")
-        return ["x": 0, "y": 0]
+        return nil
     }
     
     guard p2?["y"] != nil else {
         print("Must pass valid dictionary")
-        return ["x": 0, "y": 0]
+        return nil
     }
     
     return ["x": p1!["x"]! - p2!["x"]!, "y" : p1!["y"]! - p2!["y"]!]
